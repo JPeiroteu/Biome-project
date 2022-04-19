80708950336b59f5af506b7dab0b808fd05c3248
JPeiroteu <79811891+JPeiroteu@users.noreply.github.com>
2022-04-19 18:23:38 +0200

    Online Feature Developed

---
+++ online/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Temperature Threshold Output Control</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php
+require 'connection.php';
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT Temperature, Humidity, Threshold FROM Room ORDER BY ID DESC LIMIT 1;";
+
+if ($result = mysqli_query($conn, $sql)) {
+  while ($row =  mysqli_fetch_assoc($result)) {
+    $row_temperature = $row["Temperature"];
+    $row_humidity = $row["Humidity"];
+    $row_threshold = $row["Threshold"];
+
+echo '
+  <h2>Temperature</h2> 
+  <h3>' . $row_temperature . ' &deg;C</h3>
+  <h2>Humidity</h2> 
+  <h3>' . $row_humidity . '  &percnt;</h3>
+  <h2>ESP Arm Trigger</h2>
+  <form action="threshold_insert.php" method="post">
+    Temperature Threshold <input type="number" step="0.1" name="threshold" value="' . $row_threshold . '" required><br>
+    <input type="submit" name="submit" value="Submit">
+  </form>';
+  }
+  $result->free();
+}
+$conn->close();
+?> 
+</body>
+</html>
