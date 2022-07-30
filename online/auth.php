7377b1d794fd87b930ac5224a9dfed9169d40aec
JPeyroteo <jorgeguedes16@gmail.com>
2022-07-30 19:07:33 +0200

    password encryption changed

--- online/auth.php
+++ online/auth.php
@@ -6,18 +6,21 @@ session_start(); //important if you want to use the SESSION data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["pasword"];
-    $password = sha1($password);
     
-    $stmt = $conn->prepare("SELECT ID, Email FROM User WHERE Email = ? AND Password = ? LIMIT 1"); //stmt prepared statement, without it the webapp is vulnerable to SQL Injection
-    $stmt->bind_param("ss", $email, $password);
+    $stmt = $conn->prepare("SELECT ID, Email FROM User WHERE Email = ? LIMIT 1"); //stmt - prepared statement, without it the webapp is vulnerable to SQL Injection
+    $stmt->bind_param("s", $email);
     $stmt->execute();
-    $stmt->bind_result($id, $email);
-    $stmt->store_result();
-    
-    if($stmt->num_rows == 1) {
-        if($stmt->fetch()) {
-            $_SESSION['id'] = $id;
-            $_SESSION['email'] = $email;
+    $result = $stmt->get_result();
+
+    if ($row = $stmt->fetch()) {
+        $passwordCheck = password_verify($password, $row['Password']);
+
+        if ($passwordCheck == false) {
+            header("Location: ./");
+        }
+        else {
+            $_SESSION['id'] = $row['ID'];
+            $_SESSION['email'] = $row['Email'];
             header("Location: home.php");
         }
     }
@@ -33,11 +36,8 @@ else {
 $conn->close();
 
 /* 
-//insert new user (sha1)
-sql = "INSERT INTO User (Email, Password) VALUES ('guest.test@code.berlin', sha1(123))";
-
 //insert new user - this option is not configured
 $hashPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-sql = "INSERT INTO User (Email, Password) VALUES ('test@test.com', '$hashPassword')"; 123 password
+sql = "INSERT INTO User (Email, Password) VALUES ('guest.test@code.berlin', '$hashPassword')"; //123 password
 */
 ?> 
